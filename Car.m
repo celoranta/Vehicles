cee75dc481aebfe1d8ed2753c92018ebe718bad2
Christoper <celoranta@gmail.com>
2018-02-19 07:25:56 -0800

    complete basic 'vehicles' tutorial.

---
+++ Car.m
@@ -0,0 +1,22 @@
+//
+//  Car.m
+//  Vehicles
+//
+//  Created by Chris Eloranta on 2018-02-19.
+//  Copyright © 2018 Designated Nerd Software. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+- (id)init
+{
+    if (self = [super init]) {
+        // Since all cars have four wheels, we can safely set this for every initialized instance
+        // of a car.
+        self.numberOfWheels = 4;
+    }
+    return self;
+}
+@end
